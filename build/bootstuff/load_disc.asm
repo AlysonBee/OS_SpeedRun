6c48f467abaa0344b3364f94f39f6351524414b5
AlysonBee <alyson.stars@yahoo.com>
2020-12-08 09:32:40 +0200

    adding kernel debugger

---
+++ build/bootstuff/load_disc.asm
@@ -0,0 +1,42 @@
+
+
+disk_load:
+	pusha
+	push dx
+
+	mov ah, 0x02
+	mov al, dh
+	mov cl, 0x02
+
+	mov ch, 0x00
+	mov dh, 0x00
+	
+	int 0x13
+	jc disc_error
+	pop dx
+	cmp al, dh
+	jne sectors_error
+	popa
+	ret
+
+disc_error:
+	mov bx, DISK_ERROR
+	call print
+	call print_nl
+	mov dh, ah
+	call print_hex
+	jmp disk_loop
+
+sectors_error:
+	mov bx, SECTORS_ERROR
+	call print
+
+disk_loop:
+	jmp $
+
+DISK_ERROR: db " DIsk read error", 0
+SECTORS_ERROR: db "INcorrect number of sectors", 0
+
+
+
+
